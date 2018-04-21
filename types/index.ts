bc8228e50c38c425625a9b2939f89fa148d87ca0
felixsonyusuftosin <felixsonyusuftosin@gmail.com>
2018-04-21 23:09:30 +0100

    define web worker function for api get request ~ complete

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,13 @@
  * Global type defination all type definition must be made here
  */
 
- // Generic types for objects
- export interface KeyValuePair { 
-    [key: string]: any;
- }
+// Generic types for objects
+export interface KeyValuePair {
+  [key: string]: any;
+}
+// Types for http setings usually headers and other request settings are set here
+export interface HttpSetting {
+  [Key: string]: string;
+  type: string; // Might be GET, POST , PUT, PATCH, DELETE
+  credentials?: any;
+}
